fb40762f66b907a9bdaf14508e2f02df7b9696ec
Diogo Vilela <90631238+DiogoVilela12@users.noreply.github.com>
2022-01-20 08:05:19 -0300

    Add files via upload

---
+++ app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from app.forms import UserForm
+from app.models import User
+from django.core.paginator import Paginator
+
+
+# Create your views here.
+def home(request):
+    data = {}
+    search = request.GET.get('search')
+    if search:
+        data['db'] = User.objects.filter(username__icontains=search)
+    else:
+        data['db'] = User.objects.all()
+
+    #all = User.objects.all()
+    #paginator = Paginator(all, 3)
+    #pages = request.GET.get('page')
+    #data['db'] = paginator.get_page(pages)
+    return render(request, 'index.html', data)
+
+
+def form(request):
+    data = {}
+    data['form'] = UserForm()
+    return render(request, 'form.html', data)
+
+
+def create(request):
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+
+def view(request, pk):
+    data = {}
+    data['db'] = User.objects.get(pk=pk)
+    return render(request, 'view.html', data)
+
+
+def edit(request, pk):
+    data = {}
+    data['db'] = User.objects.get(pk=pk)
+    data['form'] = UserForm(instance=data['db'])
+    return render(request, 'form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    data['db'] = User.objects.get(pk=pk)
+    form = UserForm(request.POST or None, instance=data['db'])
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+
+def delete(request, pk):
+    db = User.objects.get(pk=pk)
+    db.delete()
+    return redirect('home')
